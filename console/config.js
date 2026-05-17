3d5b312c2764180b39b6b9529066126e2e8c1a94
suntian@gmail.com <suntian@gmail.com>
2026-05-17 09:22:08 +0800

    Configure Supabase console anon key

--- console/config.js
+++ console/config.js
@@ -1,4 +1,4 @@
 window.MACLAB_CONSOLE_CONFIG = window.MACLAB_CONSOLE_CONFIG || {
   supabaseUrl: "https://bnacbrqowfhekgwtuphu.supabase.co",
-  supabaseAnonKey: ""
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuYWNicnFvd2ZoZWtnd3R1cGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NjYzMDAsImV4cCI6MjA5NDU0MjMwMH0.UV5yMab_cMAjKSpS2ov69OKK5amyk3_E2SDwMQlZAOY"
 };
